45536a7bd96e791299683ca6d4c64a54900949ea
Kurt <Kurt@Merbeth.io>
2021-09-08 10:12:50 +0200

    added standard token

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -1,1 +1,10 @@
 
+pragma solidity ^0.8.0;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract StadardToken is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initSupply);
+    }   
+}
